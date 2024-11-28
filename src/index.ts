f2c0b67b1ee8b599a73f7ef8fe3886a49a5138d6
afeiship <1290657123@qq.com>
2024-11-28 10:27:47 +0800

    feat: add defaults

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,16 @@ import ipt from 'ipt';
 
 type Items = { name: string; value: string }[] | string[];
 
+const defaults = {
+  stdin: process.stdin,
+};
+
 export = (inItems: Items, inOptions?) => {
   const items = inItems || [];
+  const opts = { ...defaults, ...inOptions };
 
   return new Promise((resolve, reject) => {
-    ipt(items, inOptions)
+    ipt(items, opts)
       .then((res) => resolve(res[0]))
       .catch(reject);
   });
